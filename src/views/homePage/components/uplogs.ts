7246d20d01cd7ac90da2cafe54667527e99aa9dc
little star <2768530278@qq.com>
2023-05-09 14:49:28 +0800

    refactor: 提取更新日志到uplogs.ts

---
+++ src/views/homePage/components/uplogs.ts
@@ -0,0 +1,36 @@
+export const logList = [
+  {
+    version: "V1.0.2",
+    items: ["1.docs: 修改README，增加gitee仓库挂件"]
+  },
+  {
+    version: "V1.0.1",
+    items: [
+      "1.chore: 修改base路径，静态网站部署gitee",
+      "2.feat: 首页增加更新日志",
+      "3.fix: 修改默认主题色",
+      "3.fix: 预设颜色选择",
+      "4.feat: 还原默认主题色"
+    ]
+  },
+  {
+    version: "V1.0.0",
+    items: [
+      "1. 大版本1.0.0发布",
+      "2. 基础框架搭建完成",
+      "3. 集成vue-i18n、Element Plus、Echarts等",
+      "4. 图标使用iconfont、iconify",
+      "5. WyWitch 开关组件",
+      "6. WyDarkToogle 明暗模式切换开关",
+      "7. WyToolTip 自动判断文本是否溢出显示 tooltip 组件",
+      "8. WyPopoverList 悬浮列表组件",
+      "9. WyFan 扇子组件",
+      "10. WyIconFont iconfont组件",
+      "11. WyIconify iconify组件",
+      "12. 按钮自动loading Hook",
+      "13. 主题色功能",
+      "14. 404页面",
+      "15. 500页面"
+    ]
+  }
+];
