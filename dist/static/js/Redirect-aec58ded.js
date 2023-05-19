bfda5fc0e5600d274eb7ca87ad12a7cedd159a4d
little star <2768530278@qq.com>
2023-05-19 11:14:02 +0800

    chore: 生产只有打包提交，不需要通过lint-staged对打包后的文件进行格式化 && build: 打包

--- dist/static/js/Redirect-aec58ded.js
+++ dist/static/js/Redirect-aec58ded.js
@@ -1,13 +1,1 @@
-import { f as c, u as o, O as n, B as p, o as u } from "./.pnpm-c4ae2d5d.js";
-const _ = { class: "redirect-page" },
-  l = c({
-    name: "Redirect",
-    __name: "Redirect",
-    setup(i) {
-      const { currentRoute: e, replace: t } = o(),
-        { params: r, query: a } = n(e),
-        { path: s } = r;
-      return t({ path: "/" + s, query: a }), (m, d) => (u(), p("div", _));
-    }
-  });
-export { l as default };
+import{f as c,u as o,O as n,B as p,o as u}from"./.pnpm-c4ae2d5d.js";const _={class:"redirect-page"},l=c({name:"Redirect",__name:"Redirect",setup(i){const{currentRoute:e,replace:t}=o(),{params:r,query:a}=n(e),{path:s}=r;return t({path:"/"+s,query:a}),(m,d)=>(u(),p("div",_))}});export{l as default};
--- dist/static/js/index-bbfdc4c8.js
+++ dist/static/js/index-bbfdc4c8.js
@@ -1,2 +1,1 @@
-const e = Symbol();
-export { e as m };
+const e=Symbol();export{e as m};
