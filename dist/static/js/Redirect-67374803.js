268dd4bef3d911db525d22616ddfc0741698836e
little star <2768530278@qq.com>
2023-05-26 17:23:11 +0800

    build: 打包

---
+++ dist/static/js/Redirect-67374803.js
@@ -0,0 +1,1 @@
+import{f as c,u as o,O as n,B as p,o as u}from"./.pnpm-2cbd34b1.js";const _={class:"redirect-page"},l=c({name:"Redirect",__name:"Redirect",setup(i){const{currentRoute:e,replace:t}=o(),{params:r,query:a}=n(e),{path:s}=r;return t({path:"/"+s,query:a}),(m,d)=>(u(),p("div",_))}});export{l as default};
